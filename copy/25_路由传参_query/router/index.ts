7f6ac2114d0d130e0b7940d2698cce104f261dbb
gaowqspace <manba1212@163.com>
2025-11-04 17:27:20 +0800

    router-2-copy

---
+++ copy/25_路由传参_query/router/index.ts
@@ -0,0 +1,42 @@
+// 创建一个路由器，并暴露出去
+
+// 第一步：引入createRouter
+import { createRouter, createWebHistory } from 'vue-router';
+
+// 引入可能需要呈现的组件
+import Home from '@/pages/Home.vue';
+import About from '@/pages/About.vue';
+import News from '@/pages/News.vue';
+import Detail from '@/pages/Detail.vue';
+
+// 第二步：创建路由器router
+const router = createRouter({
+    history: createWebHistory(),  // 路由器的工作模式
+    routes: [
+        {
+            name:'zhuye',
+            path: '/home',
+            component: Home
+        },
+        {
+            name:'xinwen',
+            path: '/news',
+            component: News,
+            children:[
+                {
+                    name:'xiang',
+                    path:'detail',
+                    component:Detail
+                }
+            ]
+        },
+        {
+            name:'guanyu',
+            path: '/about',
+            component: About
+        },
+    ]
+})
+
+// 把路由器暴露出去
+export default router;
