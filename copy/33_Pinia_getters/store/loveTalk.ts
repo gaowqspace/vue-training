1add1bebe34c698aecf054ed72d9bb557e688fb4
gaowqspace <manba1212@163.com>
2025-11-05 18:38:14 +0800

    Pinia-6-getters

---
+++ copy/33_Pinia_getters/store/loveTalk.ts
@@ -0,0 +1,28 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import { nanoid } from "nanoid";
+
+export const useLoveTalkStore = defineStore('loveTalk', {
+    actions: {
+        async getATalk() {
+            // 发请求，下面这行的写法是连续解构赋值+重命名
+            let { data: { data: { content: title } } } = await axios.get('https://v2.jinrishici.com/one.json')
+            // console.log(result.data.data.content);
+
+            // 把请求回来的字符串包装成一个对象
+            let obj = { id: nanoid(), title };
+            // console.log(obj);
+            // 放在数组中
+            this.talkList.unshift(obj);
+        }
+    },
+    state() {
+        return {
+            talkList: [
+                { id: 't01', title: '多喝热水，少熬夜' },
+                { id: 't02', title: '代码要写得优雅' },
+                { id: 't03', title: '保持学习的习惯' },
+            ]
+        }
+    }
+})
